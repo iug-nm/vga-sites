0b3bd6069b2495d429e1bac3771580be9caee3a4
MANCHON Guilhem <gmanchon@valdegaronne.fr>
2025-05-16 10:56:23 +0200

    Modification render.php ajout elements html

--- src/equipes/render.php
+++ src/equipes/render.php
@@ -6,10 +6,10 @@
 <div <?php echo get_block_wrapper_attributes(); ?> tabindex=0>
     <?php 
     if (isset($attributes['name']) && $attributes['name']) {
-        echo $attributes['name']."<br>";
+        echo "<h4>".$attributes['name']."</h4>";
     }
     if (isset($attributes['rank']) && $attributes['rank']) {
-        echo $attributes['rank']."<br>";
+        echo "<p>".$attributes['rank']."</p>";
     }
     if (isset($attributes['picture']) && $attributes['picture']) {
         echo wp_get_attachment_image($attributes['picture'], 'full');
